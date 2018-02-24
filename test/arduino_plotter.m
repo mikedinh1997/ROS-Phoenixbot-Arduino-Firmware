5fcc61bcb4efa1917a2404c6e1b5978016654856
BryanOrabutt <borabut@sie.edu>
2018-02-23 18:22:51 -0600

    Updated octave script to plot better.

--- test/arduino_plotter.m
+++ test/arduino_plotter.m
@@ -1,6 +1,9 @@
 pkg load instrument-control
 
-s1 = serial("/dev/ttyACM0", 115200, 1) 
+clear all
+
+s1 = serial("/dev/ttyUSB0", 115200) 
+set(s1, 'timeout', -1)
 
 pid_data = cell(10,1)
 pid_plot = 0
@@ -8,13 +11,14 @@ pid_sp = 5500.0
 
 while true    
   for i = 1:10
-    data_serial = str2double(char(srl_read(s1,50)))
-    pid_data{i,1} = data_serial
+    data_serial = srl_read(s1,4)
+    data_float = typecast(uint8(data_serial), 'single')
+    pid_data{i,1} = data_float
   endfor
   pid_plot = cat(1, pid_plot, pid_data{1:10})
   plot(pid_plot)
-  hold on
-  hline(pid_sp)
+  %hold on
+  %hline(pid_sp)
   pause(1)
 endwhile
 srl_close(s1)
