bbd0424aeecf8aed5841b55fc1c852c2bbc6cc95
BryanOrabutt <borabut@siue.edu>
2018-02-26 12:55:04 -0600

    Fixed SP not plotting in Octave.

--- test/arduino_plotter.m
+++ test/arduino_plotter.m
@@ -7,15 +7,17 @@ set(s1, 'timeout', -1);
 srl_flush(s1);
 
 pid_plot = 0;
+sp_plot = 0;
 pid_sp = 0;
 
 while true    
   data_serial = srl_read(s1,4)
   data_float = typecast(uint8(data_serial), 'single')
   pid_plot = cat(1, pid_plot, data_float)
+  sp_plot= cat(1, sp_plot, pid_sp);
   plot(pid_plot, "color", "b")
   hold on
-  plot([pid_sp,pid_sp], "color", "r")
+  plot(sp_plot, "color", "r")
   drawnow()
   pause(.0001)
 endwhile
