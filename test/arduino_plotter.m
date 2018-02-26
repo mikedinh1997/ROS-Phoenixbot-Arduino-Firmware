fc8b664975db3cf0f3ec5db1ddee6accb12f0d47
BryanOrabutt <borabut@siue.edu>
2018-02-26 12:35:51 -0600

    Fixed Octave script. Plot now updates properly assuming Arduino and Octave script can be synchronized.

--- test/arduino_plotter.m
+++ test/arduino_plotter.m
@@ -2,23 +2,21 @@ pkg load instrument-control
 
 clear all
 
-s1 = serial("/dev/ttyUSB0", 115200) 
-set(s1, 'timeout', -1)
+s1 = serial("COM4", 115200);
+set(s1, 'timeout', -1);
+srl_flush(s1);
 
-pid_data = cell(10,1)
-pid_plot = 0
-pid_sp = 5500.0
+pid_plot = 0;
+pid_sp = 0;
 
 while true    
-  for i = 1:10
-    data_serial = srl_read(s1,4)
-    data_float = typecast(uint8(data_serial), 'single')
-    pid_data{i,1} = data_float
-  endfor
-  pid_plot = cat(1, pid_plot, pid_data{1:10})
-  plot(pid_plot)
-  %hold on
-  %hline(pid_sp)
-  pause(1)
+  data_serial = srl_read(s1,4)
+  data_float = typecast(uint8(data_serial), 'single')
+  pid_plot = cat(1, pid_plot, data_float)
+  plot(pid_plot, "color", "b")
+  hold on
+  plot([pid_sp,pid_sp], "color", "r")
+  drawnow()
+  pause(.0001)
 endwhile
 srl_close(s1)
